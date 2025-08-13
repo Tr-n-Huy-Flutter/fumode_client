a44ceec9a8bce93fa00a7949cafe7c377b09e9e6
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-13 23:51:41 +0700

    feat: first init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fumode_ui/fumode_ui.dart' as ui;
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,32 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(
-        ui.DevicesConstant.designDeviceWidth,
-        ui.DevicesConstant.designDeviceHeight,
-      ),
-      minTextAdapt: true,
+      designSize: const Size(375, 812),
+      useInheritedMediaQuery: true,
       splitScreenMode: true,
-      child: MaterialApp(
-        title: 'Fumode',
-        debugShowCheckedModeBanner: false,
-        theme: ui.ThemeConfig.defaultLight,
-        home: Center(
-          child: Container(
-            color: ui.AppColors.primaryOrange,
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Fumode',
+          debugShowCheckedModeBanner: false,
+          theme: ui.ThemeConfig.defaultLight,
+          home: child,
+        );
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
             child: Text(
               'Hello, Fumode UI!',
               style: TextStyle(
-                color: ui.AppColors.gray0,
-                fontSize: 24.sp,
+                color: ui.AppColors.primaryOrange,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
-            ),),
+            ),
           ),
         ),
-
-
+      ),
     );
   }
 }
-
